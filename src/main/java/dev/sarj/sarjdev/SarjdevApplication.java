96e3cf7b9a1a839ffbeb1b5d2bcbecd5ae05292e
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2023-08-19 01:19:37 +0300

    fixed incompatible Spring Boot version with Elastic client 8.9.0 version.

--- src/main/java/dev/sarj/sarjdev/SarjdevApplication.java
+++ src/main/java/dev/sarj/sarjdev/SarjdevApplication.java
@@ -3,11 +3,14 @@ package dev.sarj.sarjdev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * <a href="https://discuss.elastic.co/t/caused-by-java-lang-nosuchmethoderror-void-co-elastic-clients-transport-rest-client-restclienttransport-init/339573/9">...</a>
+ */
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration.class)
 public class SarjdevApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SarjdevApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SarjdevApplication.class, args);
+    }
 
 }

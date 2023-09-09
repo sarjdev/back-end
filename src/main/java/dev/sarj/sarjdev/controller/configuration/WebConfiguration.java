ab1e7939395212d7d166cf858106b8d74c56b0c0
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2023-09-09 14:27:40 +0300

    refactoring, changed disable cors implementation.

--- src/main/java/dev/sarj/sarjdev/controller/configuration/WebConfiguration.java
+++ src/main/java/dev/sarj/sarjdev/controller/configuration/WebConfiguration.java
@@ -1,13 +1,24 @@
 package dev.sarj.sarjdev.controller.configuration;
 
+import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+public class WebConfiguration {
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*")
+                        .allowCredentials(true)
+                        .allowedHeaders("*");
+            }
+        };
     }
 }

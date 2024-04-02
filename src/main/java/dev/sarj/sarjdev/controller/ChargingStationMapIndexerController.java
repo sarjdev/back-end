79672f34cac44e7fe62e8bcb152530c2bb225701
Yusuf Yılmaz <yusufyilmazfr@gmail.com>
2024-04-02 05:00:39 +0300

    hidden settings end-points.

--- src/main/java/dev/sarj/sarjdev/controller/ChargingStationMapIndexerController.java
+++ src/main/java/dev/sarj/sarjdev/controller/ChargingStationMapIndexerController.java
@@ -1,6 +1,7 @@
 package dev.sarj.sarjdev.controller;
 
 import dev.sarj.sarjdev.service.elasticsearch.indexer.template.ElasticSearchIndexerTemplate;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Hidden
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/charging-map-indexer")
--- src/main/java/dev/sarj/sarjdev/controller/IndicesSettingsController.java
+++ src/main/java/dev/sarj/sarjdev/controller/IndicesSettingsController.java
@@ -1,12 +1,14 @@
 package dev.sarj.sarjdev.controller;
 
 import dev.sarj.sarjdev.service.elasticsearch.settings.ElasticSearchIndicesSettingsService;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/indices-settings")
